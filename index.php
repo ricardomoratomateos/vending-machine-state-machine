67fa949c2f1819bbabdae59c11cf8ac5102ad8e3
ricardomoratomateos <ricardomoratomateos@outlook.com>
2021-01-13 18:29:25 +0100

    upload tests

--- index.php
+++ index.php
@@ -6,21 +6,25 @@ require __DIR__ . '/vendor/autoload.php';
 
 $vendingMachine = new VendingMachine();
 
-$vendingMachine->insertCoinTransaction(1.00);
-$vendingMachine->insertCoinTransaction(0.05);
-$vendingMachine->insertCoinTransaction(0.05);
-$vendingMachine->insertCoinTransaction(0.05);
-$vendingMachine->dispenseItemTransaction(VendingMachine::ITEM_CODE_JUICE);
+$vendingMachine->insertCoin(1.00);
+$vendingMachine->insertCoin(0.05);
+$vendingMachine->insertCoin(0.05);
+$vendingMachine->insertCoin(0.05);
+$vendingMachine->dispenseItem(VendingMachine::ITEM_CODE_JUICE);
 
 echo implode(', ', $vendingMachine->getActions());
 $vendingMachine->resetActions();
 
 echo "\n";
 
-$vendingMachine->insertCoinTransaction(0.05);
-$vendingMachine->insertCoinTransaction(0.05);
+$vendingMachine->insertCoin(0.05);
+$vendingMachine->insertCoin(0.05);
 $vendingMachine->cancelTransaction();
 
 echo implode(', ', $vendingMachine->getActions());
+$vendingMachine->resetActions();
+
+echo "\n";
+$vendingMachine->dispenseItem(VendingMachine::ITEM_CODE_JUICE);
 
 echo "\n";
